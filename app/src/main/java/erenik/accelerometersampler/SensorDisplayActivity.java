d038fd9d239315bcd6e06dac44d862034a39d45e
Emil Hedemalm <emil_hedemalm@hotmail.com>
2017-01-20 14:03:48 +0000

    Gotten simple location to work for 1 update at least

--- app/src/main/java/erenik/accelerometersampler/SensorDisplayActivity.java
+++ app/src/main/java/erenik/accelerometersampler/SensorDisplayActivity.java
@@ -1,19 +1,26 @@
 package erenik.accelerometersampler;
 
+import android.*;
+import android.Manifest;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.GpsStatus;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.support.v4.content.LocalBroadcastManager;
@@ -29,11 +36,15 @@ import android.os.Handler;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.ActivityRecognition;
 import com.google.android.gms.location.ActivityRecognitionApi;
 import com.google.android.gms.location.DetectedActivity;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.Viewport;
 import com.jjoe64.graphview.helper.StaticLabelsFormatter;
@@ -48,15 +59,14 @@ import java.util.List;
 /** Ref src code for Google play services Activity recognition API
  https://github.com/googlesamples/android-play-location/tree/master/ActivityRecognition
  * https://github.com/googlesamples/android-play-location/tree/master/ActivityRecognition/app/src/main/java/com/google/android/gms/location/sample/activityrecognition
-*/
+ */
 
 public class SensorDisplayActivity
         extends AppCompatActivity
         implements SensorEventListener,
         GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener,
-        ResultCallback<Status>
-{
+        ResultCallback<Status> {
     protected static final String TAG = "MainActivity";
     /**
      * A receiver for DetectedActivity objects broadcast by the
@@ -83,33 +93,24 @@ public class SensorDisplayActivity
     private DetectedActivitiesAdapter mAdapter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         System.out.println("Starting up AccelerometerSampler application");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensor_display);
 
-        Button fab = (Button) findViewById(R.id.buttonUploadToServer);
-        /// Send data to Server? Store locally?
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                TextView tv = (TextView) findViewById(R.id.PrimaryTextView);
-                tv.setText("Very snackyyy " + timesClicked++);
-            }
-        });
         findViewById(R.id.button_Restart).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 sensorChanges = 0;
                 accPoints.clear();
+                locationSamples.clear();
             }
         });
         findViewById(R.id.button_StartStop).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 on = !on;
-                ((TextView)v).setText(on? "Stop" : "Start");
+                ((TextView) v).setText(on ? "Stop" : "Start");
             }
         });
 
@@ -129,6 +130,127 @@ public class SensorDisplayActivity
         }, delay);
     }
 
+    /// Location code.
+    LocationManager lMan;
+    private class LocationData {
+        LocationData(int timestampSystemSeconds, double longitude, double latitude) {
+            this.timestampSystemSeconds = timestampSystemSeconds;
+            this.longitude = longitude;
+            this.latitude = latitude;
+        }
+        int timestampSystemSeconds; // System time ms?
+        double longitude, latitude;
+    };
+    List<LocationData> locationSamples = new ArrayList<>();
+
+    private void InitLocationUpdates() {
+        System.out.println("InitLocationUpdates");
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            System.out.println("Lacking permissions to request location updates.");
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, LocationRequest.create(), new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                // Add the data into the graph?
+                System.out.println("Location changed: "+location.getLatitude()+" "+location.getLongitude());
+                ((TextView)findViewById(R.id.textView_GPSLocation)).setText(location.getLatitude()+"lat "+location.getLongitude()+"long");
+                locationSamples.add(new LocationData((int) (System.currentTimeMillis() / 1000), location.getLatitude(), location.getLongitude()));
+                UpdateLocationGraph();
+            }
+        });
+
+//        InitGPS();
+    }
+
+    void UpdateLocationGraph() {
+        ArrayList<LineGraphSeries<DataPoint>> series = new ArrayList<>();
+        LineGraphSeries<DataPoint> longitudeSeries = new LineGraphSeries<>(),
+                latitudeSeries = new LineGraphSeries<>();
+
+        GraphView graph = (GraphView) findViewById(R.id.graphGPS);
+        graph.removeAllSeries(); // Clear old data.
+
+        // use static labels for horizontal and vertical labels
+        StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(graph);
+        staticLabelsFormatter.setHorizontalLabels(new String[] {"old", "newest"});
+        graph.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
+
+        Viewport vp = graph.getViewport();
+//        vp.setYAxisBoundsManual(true); vp.setMaxY(bounds); vp.setMinY(-bounds);
+
+        // Always reduce bounds to fit the view if possible.
+        int maxPoints = 20;
+        int firstIndex = (locationSamples.size() - maxPoints) > 0? locationSamples.size() - maxPoints : 0; // First index if using number of points.
+        int sampleNumber = 0;
+        int firstX = -1, lastX = -1;
+        float largestAbsVal = 0;
+        for (int i = firstIndex; i < locationSamples.size(); ++i)
+        {
+            LocationData ld = locationSamples.get(i);
+            // timediff in nanosecs: sd.timestamp - firstTimeStamp
+            int x = (int) ld.timestampSystemSeconds;
+            if (firstX == -1)
+                firstX = x;
+            lastX = x;
+            longitudeSeries.appendData(new DataPoint(x, ld.longitude), false, 1000, true);
+            latitudeSeries.appendData(new DataPoint(x, ld.latitude), false, 1000, true);
+        }
+        // Move towards the highest value + 1 always?
+        bounds = 0.95f * bounds + 0.05f * (largestAbsVal + 3);
+  //      vp.setXAxisBoundsManual(true);
+    //    vp.setMinX(firstX); // Set min/max X
+      //  vp.setMaxX(lastX);
+        // 0 to 25, not being displayed...
+        TextView tv = (TextView) findViewById(R.id.textView_Bounds);
+        tv.setText("firstX: "+firstX+" lastX: "+lastX+" indices added: "+(accPoints.size() - firstIndex));
+
+        longitudeSeries.setColor(0xFFFF0000);
+        latitudeSeries.setColor(0xFF00FF00);
+        graph.addSeries(longitudeSeries);
+        graph.addSeries(latitudeSeries);
+    }
+
+    void InitGPS()
+    {
+        // Android non-google Location
+        lMan = (LocationManager) getBaseContext().getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+            return;
+        }
+        lMan.addNmeaListener(new GpsStatus.NmeaListener() {
+            @Override
+            public void onNmeaReceived(long l, String s) {
+                System.out.println("Long: " + l + " s: " + s);
+            }
+        });
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        lMan.addNmeaListener(new GpsStatus.NmeaListener() {
+            @Override
+            public void onNmeaReceived(long l, String s) {
+                System.out.println("Long: " + l + " s: " + s);
+            }
+        });
+
+    }
+
     synchronized void InitGoogleAPI()
     {
         System.out.println("Init google API for activity recognition.");
@@ -136,33 +258,48 @@ public class SensorDisplayActivity
 
         // Set up Google Play Services for Activity regognition?
         mGoogleApiClient  =  new GoogleApiClient.Builder(context)
-                .addApi(ActivityRecognition.API)
+                .addApi(ActivityRecognition.API) // For Activity recognition.
+                .addApi(LocationServices.API) // For location services
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
                 .build();
 
-        mGoogleApiClient.connect();
-        // Connect it.
-
-
-        // Request updates.
-//        ActivityRecognitionApi.requestActivityUpdates
+        mGoogleApiClient.connect(); // Start services upon successful connection.
     }
+
+    /// Handle changes to the Google Services API connection.
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        System.out.println("onConnected");
+        System.out.println("- connected? "+mGoogleApiClient.isConnected());
+        requestActivityUpdates(); // Request Google activity recognition
+        InitLocationUpdates();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        System.out.println("onConnectionSuspended ");
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        System.out.println("onConnectionFailed");
+    }
+
 
     /**
      * Gets a PendingIntent to be sent for each activity detection.
      */
     private PendingIntent getActivityDetectionPendingIntent() {
         Intent intent = new Intent(this, DetectedActivitiesIntentService.class);
-
         // We use FLAG_UPDATE_CURRENT so that we get the same pending intent back when calling
         // requestActivityUpdates() and removeActivityUpdates().
         return PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
     }
 
     // Request activity updates
-    public void requestActivityUpdates()
-    {
+    public void requestActivityUpdates() {
         if (!mGoogleApiClient.isConnected()) {
             System.out.println("requestActivityUpdates - Not connected");
             Toast.makeText(this, "Not connected.",
@@ -178,14 +315,10 @@ public class SensorDisplayActivity
     }
 
     int iterated = 0;
-    public void Iterate()
-    {
-        /// Sample accelerometer
-
+    public void Iterate() {
         /// Update text
         TextView tv = (TextView) findViewById(R.id.PrimaryTextView);
-        tv.setText("Very snackyyy "+timesClicked+" iterated: "+iterated++);
-
+        tv.setText("tc "+timesClicked+" iterated: "+iterated++);
         /// Query next sampling.
         h.postDelayed(new Runnable() {
             @Override
@@ -195,6 +328,7 @@ public class SensorDisplayActivity
         }, delay);
     }
 
+    /// Application state changes - reconnect/disconnect google services and other sensors?
     @Override
     protected void onStart() {
         super.onStart();
@@ -240,8 +374,8 @@ public class SensorDisplayActivity
 
     /// All stored accelerometer-sensor points.
     ArrayList<SensorData> accPoints = new ArrayList<>();
-
     long lastGraphUpdateMs = System.currentTimeMillis();
+
     @Override
     public void onSensorChanged(SensorEvent event)
     {
@@ -365,24 +499,6 @@ public class SensorDisplayActivity
         /// Oioi.
         // D:
         System.out.println("Accuracy changed");
-    }
-
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        System.out.println("onConnected");
-        System.out.println("- connected? "+mGoogleApiClient.isConnected());
-        requestActivityUpdates(); // Request Google activity recognition
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-        System.out.println("onConnectionSuspended ");
-
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        System.out.println("onConnectionFailed");
     }
 
     private SharedPreferences getSharedPreferencesInstance() {
